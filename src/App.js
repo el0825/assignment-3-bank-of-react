fb33e0511e04066d9768a8342f42ef90a988470c
Eric Lin <eric.lin67@myhunter.cuny.edu>
2025-10-29 11:51:18 -0400

    Did some minor fixes to ensure Credits.js works properly

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.credits} addCredit={this.addCredit} accountBalance={this.state.accountBalance}/>) 
+    const CreditsComponent = () => (<Credits credits={this.state.credits} addCredit={this.addCredit} accountBalance={this.state.accountBalance} />) 
     const DebitsComponent = () => (<Debits debits={this.state.debits} />) 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
